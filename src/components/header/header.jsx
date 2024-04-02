8ff9cbd73792dfc85b8454821f27f7094cce59a3
NazarSak <112251832+NazarSak@users.noreply.github.com>
2024-04-02 16:04:57 +0300

    finish

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,10 +1,13 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import MobileMenu from '../mobileMenu/mobileMenu';
 import { headerNavigation } from '@/utils/navigations';
 import logo from '@/images/home-page/logo.png';
+import ArrowHeader from '@/images/app-page/icons/arrowHeader.svg';
+import CallPhone from '@/images/app-page/icons/callPhone.svg';
 import { Container } from '@/components/container/container';
 import { PayFineButton } from '@/components/buttons/payFineButton';
 import { usePathname } from 'next/navigation';
@@ -12,13 +15,23 @@ import cl from 'classnames';
 import { useClientTranslation } from '@/app/i18n/client';
 
 const Header = ({ lang }) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const { t } = useClientTranslation(lang, 'header');
   const pathname = usePathname();
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  console.log(isMobileMenuOpen);
+
+  const IsMobile = window.innerWidth <= 800;
+
   return (
     <header
-      className={`fixed top-0 z-[1000] flex h-[75px] w-full bg-[#F5F7F9]/80  backdrop-blur-xl duration-300`}>
+      className={`fixed top-0 z-[1000] flex h-[75px]   bg-[#F5F7F9]/80  backdrop-blur-xl duration-300 ${IsMobile ? 'w-[375px]' : 'w-full'} `}>
       <Container>
-        <div className='flex'>
+        <div className={`flex ${IsMobile ? 'justify-between' : ''}`}>
           <div className='flex items-center'>
             <Image
               src={logo}
@@ -27,6 +40,7 @@ const Header = ({ lang }) => {
               alt='Falsch-parker logo'
             />
           </div>
+
           <div className='flex w-full items-center justify-center max-[950px]:hidden'>
             <ul className='flex w-full max-w-[650px] justify-between'>
               {headerNavigation.map((item) => (
@@ -45,8 +59,34 @@ const Header = ({ lang }) => {
               ))}
             </ul>
           </div>
-          <PayFineButton text='Pay fine' />
+
+          {!IsMobile && <PayFineButton text='Pay fine' />}
+
+          <div
+            className={`flex items-center gap-4 ${IsMobile ? '' : 'hidden'}`}>
+            <button onClick={toggleMobileMenu}>
+              <Image
+                src={ArrowHeader}
+                alt='ArrowHeader'
+                style={{
+                  transform: isMobileMenuOpen
+                    ? 'rotate(180deg)'
+                    : 'rotate(0deg)',
+                  transition: 'transform 0.4s ease-in-out',
+                }}
+              />
+            </button>
+            <button className='flex items-center justify-center gap-2 rounded-full bg-[#05D54B] p-2'>
+              <Image src={CallPhone} alt='CallPhone' />
+            </button>
+          </div>
         </div>
+
+        <MobileMenu
+          isOpen={isMobileMenuOpen}
+          onClose={toggleMobileMenu}
+          lang={lang}
+        />
       </Container>
     </header>
   );
